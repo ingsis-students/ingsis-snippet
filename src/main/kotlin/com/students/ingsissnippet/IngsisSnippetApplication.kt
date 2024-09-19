03db6b5c78dcedeafb5ccf68430de8706789e361
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-09-19 17:00:47 -0300

    First Commit ~ Project Setup

---
+++ src/main/kotlin/com/students/ingsissnippet/IngsisSnippetApplication.kt
@@ -0,0 +1,11 @@
+package com.students.ingsissnippet
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class IngsisSnippetApplication
+
+fun main(args: Array<String>) {
+	runApplication<IngsisSnippetApplication>(*args)
+}
