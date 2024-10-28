2692ad2cc334d44253d8e91664c21b9b2afd31df
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-10-28 18:45:37 -0300

    snippet repository first approach

--- src/main/kotlin/com/students/ingsissnippet/repositories/SnippetRepository.kt
+++ src/main/kotlin/com/students/ingsissnippet/repositories/SnippetRepository.kt
@@ -1,8 +1,13 @@
 package com.students.ingsissnippet.repositories
 
 import com.students.ingsissnippet.entities.Snippet
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface SnippetRepository : JpaRepository<Snippet, Long>
+interface SnippetRepository : JpaRepository<Snippet, Long> {
+    fun findByNameContainingIgnoreCase(name: String, pageable: Pageable): Page<Snippet>
+    fun countByNameContainingIgnoreCase(name: String): Long
+}
