b602436f4f22814afe354123e22ab510b889752a
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-10-31 18:03:26 -0300

    Test DTO first approach

---
+++ src/main/kotlin/com/students/ingsissnippet/dtos/response_dtos/TestDTO.kt
@@ -0,0 +1,9 @@
+package com.students.ingsissnippet.dtos.response_dtos
+
+import com.students.ingsissnippet.entities.Test
+
+class TestDTO(test: Test){
+    val id: Long = test.id
+    val input: String = test.input
+    val output: String = test.output
+}
