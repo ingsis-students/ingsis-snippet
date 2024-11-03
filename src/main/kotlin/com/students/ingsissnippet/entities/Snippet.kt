b6833e172574605b54a220b6ecf74ba31120b409
IgnacioChevallier <ichevallierboutell@mail.austral.edu.ar>
2024-11-02 22:16:33 -0300

    Added JsonManagedReference and JsonBackReference relation on entities Snippet.kt and Test.kt

--- src/main/kotlin/com/students/ingsissnippet/entities/Snippet.kt
+++ src/main/kotlin/com/students/ingsissnippet/entities/Snippet.kt
@@ -1,7 +1,6 @@
 package com.students.ingsissnippet.entities
 
 import com.fasterxml.jackson.annotation.JsonBackReference
-import com.fasterxml.jackson.annotation.JsonManagedReference
 import com.students.ingsissnippet.dtos.request_types.Compliance
 import com.students.ingsissnippet.dtos.request_types.Compliance.PENDING
 import jakarta.persistence.Entity
@@ -27,8 +26,9 @@ data class Snippet(
     @JoinColumn(name = "language_id", nullable = false)
     @JsonBackReference
     val language: Language,
+
     @OneToMany(mappedBy = "snippet", cascade = [CascadeType.ALL], fetch = FetchType.LAZY)
-    @JsonManagedReference
+    @JsonBackReference
     val tests: List<Test> = emptyList()
 ) {
     constructor() : this(0, "", "", PENDING, Language(), emptyList())
