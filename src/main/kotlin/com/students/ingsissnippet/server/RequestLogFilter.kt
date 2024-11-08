7beb067f88558fe4f10e9e634e9dd48bd3d6e4ef
TomasValleDuran <tvalleduran@mail.austral.edu.ar>
2024-11-08 12:14:11 -0300

    improved logger

--- src/main/kotlin/com/students/ingsissnippet/server/RequestLogFilter.kt
+++ src/main/kotlin/com/students/ingsissnippet/server/RequestLogFilter.kt
@@ -1,34 +1,46 @@
 package com.students.ingsissnippet.server
 
+import jakarta.servlet.Filter
 import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletException
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
-import jakarta.servlet.Filter
 import org.slf4j.LoggerFactory
+import org.slf4j.Logger
+import org.springframework.core.annotation.Order
 import org.springframework.stereotype.Component
 import java.io.IOException
 
 @Component
+@Order(2)
 class RequestLogFilter : Filter {
-
-    private val logger = LoggerFactory.getLogger(RequestLogFilter::class.java)
-
     @Throws(IOException::class, ServletException::class)
-    override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
-        val httpRequest = request as HttpServletRequest
-        val httpResponse = response as HttpServletResponse
-        val uri = httpRequest.requestURI
-        val method = httpRequest.method
-        val prefix = "$method $uri"
+    override fun doFilter(
+        request: ServletRequest,
+        response: ServletResponse,
+        chain: FilterChain
+    ) {
+        if (request is HttpServletRequest && response is HttpServletResponse) {
+            val httpRequest = request
 
-        try {
+            val uri = httpRequest.requestURI
+            val method = httpRequest.method
+            val prefix = "$method $uri"
+
+            try {
+                chain.doFilter(request, response)
+            } finally {
+                val statusCode = response.status
+                logger.info("{} - {}", prefix, statusCode)
+            }
+        } else {
             chain.doFilter(request, response)
-        } finally {
-            val statusCode = httpResponse.status
-            logger.info("$prefix - $statusCode")
         }
+    }
+
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(RequestLogFilter::class.java)
     }
 }
