cadd8742123e63f14609001ab5b21451ac7f23b1
Santos Bogo <72747461+santosbogo@users.noreply.github.com>
2024-11-09 14:39:22 -0300

    Parse service interface

--- src/main/kotlin/com/students/ingsissnippet/routes/ParseServiceRoutes.kt
+++ src/main/kotlin/com/students/ingsissnippet/routes/ParseServiceRoutes.kt
@@ -3,6 +3,7 @@ package com.students.ingsissnippet.routes
 import com.students.ingsissnippet.dtos.request_dtos.DTO
 import com.students.ingsissnippet.dtos.response_dtos.FullSnippet
 import org.springframework.http.HttpEntity
+import org.springframework.http.ResponseEntity
 
 /** This class is intended to have an overview of the Parse Service */
 interface ParseServiceRoutes {
@@ -15,7 +16,7 @@ interface ParseServiceRoutes {
     /** Route that uses parse service to check if a snippet compiles and returns errors */
     fun validate(id: Long): String
     /** Route that uses parse service to execute a post request */
-    fun test(snippetId: Long, inputs: List<String>, outputs: List<String>): List<String>
+    fun test(token: String, snippetId: Long, inputs: List<String>, outputs: List<String>): ResponseEntity<String>
 
     fun executePost(entity: HttpEntity<DTO>, route: String): String
 }
